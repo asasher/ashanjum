e43674bd44a97d03f2ecaff41369458f40f5965f
Asher <as.asher.anjum@gmail.com>
2024-09-12 21:25:36 +0400

    Init

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js");
+import createMDX from '@next/mdx'
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+
+/** @type {import("next").NextConfig} */
+const config = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+};
+
+export default withMDX(config);
---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Asher Anjum",
+  description: "various words of varying veracity",
+  icons: [
+    {
+      rel: "icon",
+      url: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😈</text></svg>",
+    },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>
+        <main className="prose container mx-auto flex min-h-screen max-w-md flex-col items-start justify-center p-8">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
